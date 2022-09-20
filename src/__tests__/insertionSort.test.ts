9ef0468462ca5bd584305d6558e3c861badd50c5
Peter Willumsen <pewillumsen@gmail.com>
2022-09-20 13:09:15 +0200

    added insertion, selection sort

--- src/__tests__/insertionSort.test.ts
+++ src/__tests__/insertionSort.test.ts
@@ -1,15 +1,15 @@
 import {describe, expect, test} from '@jest/globals';
-import bubbleSort from '@code/bubbleSort';
+import insertionSort from '@code/insertionSort';
 
-describe('Bubblesort', () => {
+describe('insertionsort', () => {
   test('Allready sorted', () => {
     let arr = [0,2,4];
-    bubbleSort(arr);
+    insertionSort(arr);
     expect(arr).toEqual([0,2,4]);
   });
   test('Sorts correctly', () => {
     let arr = [10,-1,1111,0,13];
-    bubbleSort(arr);
+    insertionSort(arr);
     expect(arr).toEqual([-1,0,10,13,1111]);
   });
 });
---
+++ src/code/insertionSort.ts
@@ -0,0 +1,9 @@
+export default function insertionSort<T>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    let j = i;
+    while (j > 0 && arr[j] < arr[j - 1]) {
+      [arr[j], arr[j - 1]] = [arr[j - 1], arr[j]];
+      j--;
+    }
+  }
+}
