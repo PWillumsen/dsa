98585d08b8f26dca41a701530e23e109ebbba3d2
Peter Willumsen <pewillumsen@gmail.com>
2022-09-21 09:54:18 +0200

    quicksort test

---
+++ src/__tests__/quickSort.test.ts
@@ -0,0 +1,10 @@
+import {test, expect } from "@jest/globals";
+import quickSort from "@code/quickSort";
+
+test("Quick sort", function () {
+    const arr = [9, 3, -1, 7, 4, 69, 420, 42];
+
+    quickSort(arr);
+    expect(arr).toEqual([-1, 3, 4, 7, 9, 42, 69, 420]);
+});
+
